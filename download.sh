15f4e8dd778989dfac70c15004a33f0d6f5aea5a
muflax <mail@muflax.com>
2015-05-15 20:23:43 +0100

    multiple galleries

--- download.sh
+++ download.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-gallery=$1
-mkdir $gallery
+for gallery in $*; do
+  mkdir -p $gallery
 
-for ((i = 0; i <= 100; i++)); do
-  echo "grabbing page $i..."
+  for ((i = 0; i <= 100; i++)); do
+    echo "grabbing page $i..."
 
-  wget -q "http://imgur.com/r/${gallery}/page/$i" -O - | grep 'class="post"' | cut -d\" -f2 | while read id
-  do
-    echo "downloading $id.jpg"
-    wget -q -c "http://i.imgur.com/$id.jpg" -P $gallery
+    wget -q "http://imgur.com/r/${gallery}/page/$i" -O - | grep 'class="post"' | cut -d\" -f2 | while read id
+    do
+      echo "downloading $id.jpg..."
+      wget -q -c "http://i.imgur.com/$id.jpg" -P $gallery
+    done
   done
 done
